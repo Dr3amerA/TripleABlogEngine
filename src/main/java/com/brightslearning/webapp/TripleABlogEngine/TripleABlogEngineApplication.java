62ccf95beb86af27646624199730596b1f621418
Alex <burnupalext_t@hotmail.com>
2022-11-14 09:55:33 +0200

    1st commit

---
+++ src/main/java/com/brightslearning/webapp/TripleABlogEngine/TripleABlogEngineApplication.java
@@ -0,0 +1,13 @@
+package com.brightslearning.webapp.TripleABlogEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleABlogEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripleABlogEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brightslearning/webapp/TripleABlogEngine/TripleABlogEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brightslearning.webapp.TripleABlogEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripleABlogEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
